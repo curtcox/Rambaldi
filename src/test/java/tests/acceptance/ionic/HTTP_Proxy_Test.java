75f322c5e8cf7ed12ae13cd24ea5d3e4660547eb
Curt Cox <curtcox@gmail.com>
2012-10-30 20:12:54 -0500

    Added failing UAT for HTTP Proxy

---
+++ src/test/java/tests/acceptance/ionic/HTTP_Proxy_Test.java
@@ -0,0 +1,24 @@
+package tests.acceptance.ionic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class HTTP_Proxy_Test {
+
+    @Test
+    public void I_should_be_able_to_work_as_a_CERN_Proxy() {
+        fail();
+    }
+
+    @Test
+    public void I_should_be_able_to_accept_a_dynamic_proxy_function() {
+        fail();
+    }
+
+    @Test
+    public void I_should_be_able_to_proxy_service_requests_with_stateful_auth() {
+        fail();
+    }
+
+}
